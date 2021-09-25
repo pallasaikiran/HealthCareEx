77e8a1424acbc9e083ba86797ed1cad5677619ad
DESKTOP-4DON97T\Balaji <sheshrao@pipra.solutions>
2021-09-25 13:31:25 +0530

    first commit

---
+++ src/test/java/com/sai/app1/HealthCareExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sai.app1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCareExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
