43c62753131774ff4ea6dfd5c878cfad58323921
saikiran <sheshrao@pipra.solutions>
2021-09-25 14:47:53 +0530

    spec repo

---
+++ src/main/java/com/sai/app1/repository/SpecializationRepository.java
@@ -0,0 +1,9 @@
+package com.sai.app1.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sai.app1.entity.Specialization;
+
+public interface SpecializationRepository extends JpaRepository<Specialization, Long> {
+
+}
